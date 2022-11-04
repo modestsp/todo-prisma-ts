8f6396d1e88e6c2926341ff37676b80f594be321
srsebbi <csperichon@gmail.com>
2022-11-04 19:17:05 -0300

    Added bodu parser

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-import express from 'express'
-import config from 'config'
-import logger from './utils/logger'
+import express from 'express';
+import config from 'config';
+import logger from './utils/logger';
+import routes from './utils/routes';
 
-const port = config.get<number>('port')
-const app = express()
-
-app.listen(port, () => {
-  logger.info(`App is running at http://localhost:${port}`)
-})
+const port = config.get<number>('port');
+const app = express();
+app.use(express.json());
+app.listen(port, async () => {
+  logger.info(`App is running at http://localhost:${port}`);
+  routes(app);
+});

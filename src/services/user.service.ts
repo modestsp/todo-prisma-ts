8f23e1b2c1052f14ee25c2ea713b8a04fac51974
srsebbi <csperichon@gmail.com>
2022-11-04 19:18:33 -0300

    Added user service for create a user

---
+++ src/services/user.service.ts
@@ -0,0 +1,23 @@
+import { db } from '../utils/db.server';
+import bcrypt from 'bcrypt';
+import config from 'config';
+import { CreateUserInput } from '../schema/user.schema';
+
+export const createUser = async (input: CreateUserInput) => {
+  const { username, email, name, password } = input;
+  const salt = await config.get<number>('saltRounds');
+  const passwordHash = await bcrypt.hash(password, salt);
+  try {
+    const newUser = await db.user.create({
+      data: {
+        username,
+        name,
+        email,
+        password: passwordHash,
+      },
+    });
+    return { id: newUser.id, email: newUser.email };
+  } catch (e: any) {
+    throw new Error(e);
+  }
+};

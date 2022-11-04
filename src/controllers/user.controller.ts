777d381501c2d8a2fed1ab63f422ce7d207da8b4
srsebbi <csperichon@gmail.com>
2022-11-04 20:31:50 -0300

    Omitting passwordhash in response

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { CreateUserInput } from '../schema/user.schema';
 import { createUser } from '../services/user.service';
+import { omit } from 'lodash';
 import logger from '../utils/logger';
 
 export const createUserHandler = async (
@@ -9,7 +10,7 @@ export const createUserHandler = async (
 ) => {
   try {
     const user = await createUser(req.body);
-    return res.json(user);
+    return res.send(omit(user, 'password'));
   } catch (e: any) {
     logger.error(e);
     // 409 because we assum that some of the inputs are not unique

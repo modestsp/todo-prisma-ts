cf9a7940c3451b37ccd92fb49952881cf853695a
srsebbi <csperichon@gmail.com>
2022-11-05 20:41:55 -0300

    Added session routes

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,12 +1,26 @@
 import { Express, Request, Response } from 'express';
+import {
+  createUserSessionHandler,
+  getUserSessionsHandler,
+} from '../controllers/session.controller';
 import { createUserHandler } from '../controllers/user.controller';
+import requireUser from '../middleware/requireUser';
 import validateResource from '../middleware/validateResource';
+import { createSessionSchema } from '../schema/session.schema';
 import { createUserSchema } from '../schema/user.schema';
 
 function routes(app: Express) {
   app.get('/ok', (req: Request, res: Response) => res.sendStatus(200));
 
   app.post('/api/users', validateResource(createUserSchema), createUserHandler);
+
+  app.post(
+    '/api/sessions',
+    validateResource(createSessionSchema),
+    createUserSessionHandler
+  );
+
+  app.get('/api/sessions', requireUser, getUserSessionsHandler);
 }
 
 export default routes;

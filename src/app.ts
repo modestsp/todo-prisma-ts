502262caafcb2e2ee0544cec46b29f9fa2259661
srsebbi <csperichon@gmail.com>
2022-11-05 20:39:06 -0300

    Added deserialize middleware

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,16 @@ import express from 'express';
 import config from 'config';
 import logger from './utils/logger';
 import routes from './utils/routes';
+import deserializeUser from './middleware/deserializeUser';
 
 const port = config.get<number>('port');
+
 const app = express();
+
 app.use(express.json());
+
+app.use(deserializeUser);
+
 app.listen(port, async () => {
   logger.info(`App is running at http://localhost:${port}`);
   routes(app);

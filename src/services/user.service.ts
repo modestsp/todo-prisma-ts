7100e363848b0dee2f68ee9420ec6228f60c7aa2
srsebbi <csperichon@gmail.com>
2022-11-05 20:41:26 -0300

    Added valdiatePassword service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,8 +2,10 @@ import { db } from '../utils/db.server';
 import bcrypt from 'bcrypt';
 import config from 'config';
 import { CreateUserInput } from '../schema/user.schema';
+import { User } from '@prisma/client';
+import { omit } from 'lodash';
 
-export const createUser = async (input: CreateUserInput) => {
+export const createUser = async (input: CreateUserInput): Promise<User> => {
   const { username, email, name, password } = input;
   const salt = await config.get<number>('saltRounds');
   const passwordHash = await bcrypt.hash(password, salt);
@@ -20,4 +22,30 @@ export const createUser = async (input: CreateUserInput) => {
   } catch (e: any) {
     throw new Error(e);
   }
+};
+
+export const validatePassword = async ({
+  username,
+  password,
+}: {
+  username: string;
+  password: string;
+}) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+  });
+
+  if (!user) {
+    return false;
+  }
+  // Bcrypt compare
+  const passwordCorrect = await bcrypt.compare(password, user.password);
+
+  if (!passwordCorrect) {
+    return false;
+  }
+
+  return user;
 };

1773005a73049401f28dbeaa337df4f66dbc072f
srsebbi <csperichon@gmail.com>
2022-11-05 20:41:46 -0300

    Created jwt utils for sign and verify jwt

---
+++ src/utils/jwt.utils.ts
@@ -0,0 +1,40 @@
+import jwt from 'jsonwebtoken';
+import config from 'config';
+
+const privateKey = Buffer.from(
+  config.get<string>('privateKey'),
+  'base64'
+).toString('ascii');
+const publicKey = Buffer.from(
+  config.get<string>('publicKey'),
+  'base64'
+).toString('ascii');
+// Sign the jwt with a private key
+export const signJwt = (
+  object: Object,
+  options?: jwt.SignOptions | undefined
+) => {
+  return jwt.sign(object, privateKey, {
+    // Options can be undefined
+    ...(options && options),
+    algorithm: 'RS256',
+  });
+};
+
+// Verify the jwt with a public key
+export const verifyJwt = (token: string) => {
+  try {
+    const decoded = jwt.verify(token, publicKey);
+    return {
+      valid: true,
+      expired: false,
+      decoded,
+    };
+  } catch (e: any) {
+    return {
+      valid: false,
+      expired: e.message === 'jwt expired',
+      decoded: null,
+    };
+  }
+};

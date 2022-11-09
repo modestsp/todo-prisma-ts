5f6e0331d4907bb62770d7e9cc768aeecbedb811
srsebbi <csperichon@gmail.com>
2022-11-09 14:44:21 -0300

    Created todo controller, added createTodoHandler

---
+++ src/controllers/todo.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { CreateTodoInput } from '../schema/todo.schema';
+import { createTodo } from '../services/todo.service';
+import logger from '../utils/logger';
+
+export const createTodoHandler = async (
+  req: Request<{}, {}, CreateTodoInput>,
+  res: Response
+) => {
+  const currentUser = res.locals.user;
+  logger.info('CURRENT USER', currentUser);
+  try {
+    const todo = await createTodo(req.body, currentUser.id);
+    return res.send(todo);
+  } catch (e: any) {
+    logger.error(e);
+    return res.status(400).send(e.message);
+  }
+};

9955c2515ee45ac169271e2566447d988d5aa688
srsebbi <csperichon@gmail.com>
2022-11-09 14:45:17 -0300

    Added createTodo route

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -4,23 +4,29 @@ import {
   updateSessionHandler,
   getUserSessionsHandler,
 } from '../controllers/session.controller';
+import { createTodoHandler } from '../controllers/todo.controller';
 import { createUserHandler } from '../controllers/user.controller';
 import requireUser from '../middleware/requireUser';
 import validateResource from '../middleware/validateResource';
 import { createSessionSchema } from '../schema/session.schema';
+import { createTodoSchema } from '../schema/todo.schema';
 import { createUserSchema } from '../schema/user.schema';
 
 function routes(app: Express) {
   app.get('/ok', (req: Request, res: Response) => res.sendStatus(200));
 
+  // Users
   app.post('/api/users', validateResource(createUserSchema), createUserHandler);
 
+  // Todos
+  app.post('/api/todos', validateResource(createTodoSchema), createTodoHandler);
+
+  // Sessions
   app.post(
     '/api/sessions',
     validateResource(createSessionSchema),
     createUserSessionHandler
   );
-
   app.get('/api/sessions', requireUser, getUserSessionsHandler);
   app.put('/api/sessions', requireUser, updateSessionHandler);
 }

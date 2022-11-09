c2191037b3269482577b2b785de8ca0f1111ea0c
srsebbi <csperichon@gmail.com>
2022-11-09 14:48:14 -0300

    Added omit password on res.locals.user

--- src/middleware/deserializeUser.ts
+++ src/middleware/deserializeUser.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { get } from 'lodash';
+import { get, omit } from 'lodash';
 import { verifyJwt } from '../utils/jwt.utils';
 
 const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
@@ -15,7 +15,7 @@ const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
   const { decoded } = verifyJwt(accessToken);
 
   if (decoded) {
-    res.locals.user = decoded;
+    res.locals.user = omit(decoded, 'password');
     return next();
   }
 

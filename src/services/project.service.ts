4e14cb48795a4f180a564dcb24f37a20d445a7be
srsebbi <csperichon@gmail.com>
2022-11-12 18:43:59 -0300

    Create project service and added createProject

---
+++ src/services/project.service.ts
@@ -0,0 +1,22 @@
+import { User } from '@prisma/client';
+import { CreateProjectSchema } from '../schema/project.schema';
+import { db } from '../utils/db.server';
+
+export const createProject = async (
+  input: CreateProjectSchema,
+  userId: User['id']
+) => {
+  try {
+    const { title, endsAt } = input;
+    const newProject = await db.project.create({
+      data: {
+        title,
+        endsAt: new Date(endsAt),
+        creatorId: userId,
+      },
+    });
+    return newProject;
+  } catch (e: any) {
+    throw new Error(e.message);
+  }
+};

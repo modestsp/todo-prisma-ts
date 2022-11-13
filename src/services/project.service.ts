9c6c7a3f508a9c9cf1429bbd7732f376ae81ce9c
srsebbi <csperichon@gmail.com>
2022-11-13 19:19:02 -0300

    Added update and delete a project services

--- src/services/project.service.ts
+++ src/services/project.service.ts
@@ -1,9 +1,13 @@
-import { User } from '@prisma/client';
-import { CreateProjectSchema } from '../schema/project.schema';
+import { Project, User } from '@prisma/client';
+import {
+  CreateProjectInput,
+  DeleteProjectInput,
+  UpdateProjectInput,
+} from '../schema/project.schema';
 import { db } from '../utils/db.server';
 
 export const createProject = async (
-  input: CreateProjectSchema,
+  input: CreateProjectInput,
   userId: User['id']
 ) => {
   try {
@@ -27,8 +31,49 @@ export const getAllProjects = async (userId: User['id']) => {
       where: {
         creatorId: userId,
       },
+      include: {
+        Todo: {
+          select: {
+            description: true,
+          },
+        },
+      },
     });
     return projects;
+  } catch (e: any) {
+    throw new Error(e.message);
+  }
+};
+
+export const deleteProject = async (input: DeleteProjectInput) => {
+  try {
+    const { projectId } = input;
+    const deletedProject = await db.project.delete({
+      where: {
+        id: projectId,
+      },
+    });
+    return deletedProject;
+  } catch (e: any) {
+    throw new Error(e.message);
+  }
+};
+
+export const updateProject = async (input: UpdateProjectInput) => {
+  try {
+    const { projectId, title, endsAt } = input;
+    const updatedProject = await db.project.update({
+      where: {
+        id: projectId,
+      },
+      data: {
+        title,
+        endsAt,
+      },
+    });
+    console.log('ACA EL INPUT', input);
+    console.log('ACA EL Project', updatedProject);
+    return updatedProject;
   } catch (e: any) {
     throw new Error(e.message);
   }

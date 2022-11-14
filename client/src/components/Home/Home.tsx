23d47879c228a3e3b6889761528a04f11f439337
srsebbi <csperichon@gmail.com>
2022-11-14 18:34:40 -0300

    Created home page

---
+++ client/src/components/Home/Home.tsx
@@ -0,0 +1,13 @@
+import { Header } from './Header/Header';
+import { Main } from './Main/Main';
+import { Footer } from './Footer/Footer';
+
+export const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+};

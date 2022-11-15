fb6db6d20241292fab51caffdd7017ee4a62263d
srsebbi <csperichon@gmail.com>
2022-11-15 20:50:18 -0300

    Basic functionality

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,8 +1,17 @@
 import { Header } from './Header/Header';
 import { Main } from './Main/Main';
 import { Footer } from './Footer/Footer';
+import { useContext, useEffect } from 'react';
+import { UserContext } from '../../App';
 
 export const Home = () => {
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+
+  useEffect(() => {
+    console.log(currentUser);
+
+    console.log('Nuevo estate', currentUser);
+  }, []);
   return (
     <div>
       <Header />

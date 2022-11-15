fc3a0da538c3308f49511a48bc4c8037e5fb0874
srsebbi <csperichon@gmail.com>
2022-11-15 20:48:31 -0300

    Added cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,10 +3,20 @@ import config from 'config';
 import logger from './utils/logger';
 import routes from './utils/routes';
 import deserializeUser from './middleware/deserializeUser';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const port = config.get<number>('port');
 
 const app = express();
+app.use(
+  cors({
+    origin: config.get('origin'),
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
 
 app.use(express.json());
 

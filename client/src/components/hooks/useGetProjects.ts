66a400345b01ac7f8c8a1269459cd503da34b980
srsebbi <csperichon@gmail.com>
2022-11-19 14:07:12 -0300

    Create hook for fetch all projects

---
+++ client/src/components/hooks/useGetProjects.ts
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query';
+import projectService from '../../services/project.service';
+
+export const useGetProjects = () => {
+  const { isLoading, data, isError, error } = useQuery({
+    queryKey: ['projects'],
+    queryFn: projectService.getProjects,
+    onSuccess: () => {
+      console.log('Projects completed');
+    },
+  });
+
+  return { isLoading, data, isError, error };
+};

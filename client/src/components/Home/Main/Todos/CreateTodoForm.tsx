fd36a2a3cde832458a4ea46e293cfa35782345b2
srsebbi <csperichon@gmail.com>
2022-11-19 14:09:11 -0300

    Removed unused code and changed the fetch method to react query

--- client/src/components/Home/Main/Todos/CreateTodoForm.tsx
+++ client/src/components/Home/Main/Todos/CreateTodoForm.tsx
@@ -1,14 +1,11 @@
 import { object, string } from 'zod';
-import { useContext, useState } from 'react';
-import { UserContext } from '../../../../App';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { CreateTodoInput } from '../../../../types';
-import todoService from '../../../../services/todo.service';
-import { useMutation, useQuery } from '@tanstack/react-query';
 import styles from './todos.module.css';
-import userService from '../../../../services/user.service';
-
+import { useGetCurrentUser } from '../../../hooks/useGetCurrentUser';
+import { useCreateTodo } from '../../../hooks/useCreateTodo';
+import { useState } from 'react';
 export const createTodoSchema = object({
   description: string({
     required_error: 'Description is required',
@@ -19,17 +16,11 @@ export const createTodoSchema = object({
 });
 
 export const CreateTodoForm = () => {
-  // const navigate = useNavigate();
   const [errorMesage, setErrorMessage] = useState<string | null>(null);
-  const contextT = useContext(UserContext);
+  const { data: currentUser } = useGetCurrentUser();
 
-  const addTodo = useMutation({
-    mutationFn: () => {
-      return 'Helo';
-    },
-  });
+  const { mutate, isLoading, error, isError } = useCreateTodo();
 
-  // const { accessToken, setAccessToken } = useContext(UserContext);
   const {
     register,
     handleSubmit,
@@ -39,10 +30,8 @@ export const CreateTodoForm = () => {
   });
   const onSubmit: SubmitHandler<CreateTodoInput> = async (input) => {
     try {
-      const currentUser = contextT?.currentUser;
-      if (currentUser?.id) {
-        // const createdTodo = await todoService.createTodo(input, currentUser.id);
-        // console.log('NEW TODO', createdTodo);
+      if (currentUser) {
+        mutate({ input, userId: currentUser.id });
       }
     } catch (e: any) {
       setErrorMessage(e.response?.data?.error);
--- client/src/components/Home/Main/Todos/Todos.tsx
+++ client/src/components/Home/Main/Todos/Todos.tsx
@@ -1,44 +1,15 @@
-import { useContext, useEffect, useState } from 'react';
-import { UserContext } from '../../../../App';
 import { Todo } from '../../../../types';
 import todoService from '../../../../services/todo.service';
 import { DisplayTodo } from './DisplayTodo';
-import projectService from '../../../../services/project.service';
-import { useNavigate } from 'react-router-dom';
 import { CreateTodoForm } from './CreateTodoForm';
 import { useQuery } from '@tanstack/react-query';
 
 export const Todos = () => {
-  // const [todos, setTodos] = useState<Todo[] | []>([]);
-  const navigate = useNavigate();
-  const context = useContext(UserContext);
-  let userId = context?.currentUser?.id;
-
   const { isLoading, data: todos } = useQuery({
     queryKey: ['todos'],
     queryFn: todoService.getAllTodos,
   });
-  // Tal vez guardar los todos en context
-  // useEffect(() => {
-  //   const getTodos = async () => {
-  //     try {
-  //       let userId = context?.currentUser?.id;
-  //       if (userId && todos.length === 0) {
-  //         console.log('TODOS ANTES DEL FETCHING', todos);
-  //         const allTodos = await todoService.getAllTodos();
-  //         const projects = await projectService.getProjects();
-  //         console.log(projects);
-  //         setTodos(allTodos);
-  //         console.log(userId);
-  //       }
-  //     } catch (e: any) {
-  //       console.log('ERROR', e);
-  //       navigate('/auth/login');
-  //     }
-  //   };
-  //   getTodos();
-  // }, [userId]);
-  // console.log(todos);
+
   return (
     <ul>
       <button>Create a todo</button>

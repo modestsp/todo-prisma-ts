a26d0fcc0755ce24b954484ae38674ad1e7d442e
srsebbi <csperichon@gmail.com>
2022-11-19 14:10:12 -0300

    Added handleDelete funct for deleting a todo

--- client/src/components/Home/Main/Todos/DisplayTodo.tsx
+++ client/src/components/Home/Main/Todos/DisplayTodo.tsx
@@ -1,11 +1,28 @@
 import { Todo } from '../../../../types';
 import styles from './todos.module.css';
+import { useDeleteTodo } from '../../../hooks/useDeleteTodo';
 
 export const DisplayTodo = ({ todo }: { todo: Todo }) => {
+  const { mutate, isError, isLoading, error, isSuccess } = useDeleteTodo();
+  const handleDelete = () => {
+    if (window.confirm('Are you sure?')) {
+      mutate(todo.id);
+    } else {
+      console.log('Cancelled');
+    }
+  };
   return (
     <li className={styles.todoCard}>
-      <p>Description</p>
-      {todo.description} | {todo.endsAt}
+      {!isLoading ? (
+        <div>
+          {' '}
+          <p>Description</p>
+          {todo.description} | {todo.endsAt}
+          <button onClick={handleDelete}>Remove Todo</button>
+        </div>
+      ) : (
+        <p>Deleting</p>
+      )}
     </li>
   );
 };

2df83df52cbcfa6dfb42cad562cf2052aa7fd9a6
srsebbi <csperichon@gmail.com>
2022-11-19 14:09:54 -0300

    Removed unused code and refactor to use react query

--- client/src/components/Home/Main/Projects/Projects.tsx
+++ client/src/components/Home/Main/Projects/Projects.tsx
@@ -1,40 +1,14 @@
 import { Project } from '../../../../types';
-import { useContext, useState, useEffect } from 'react';
-import { UserContext } from '../../../../App';
-import projectService from '../../../../services/project.service';
 import { DisplayProject } from './DisplayProject';
-import { useNavigate } from 'react-router-dom';
+import { useGetProjects } from '../../../hooks/useGetProjects';
 
 export const Projects = () => {
-  const [projects, setProjects] = useState<Project[] | []>([]);
-  const navigate = useNavigate();
-  const context = useContext(UserContext);
-  let userId = context?.currentUser?.id;
+  const { data: projects, isError, error, isLoading } = useGetProjects();
 
-  // Tal vez guardar los todos en context
-  useEffect(() => {
-    const getTodos = async () => {
-      try {
-        let userId = context?.currentUser?.id;
-        if (userId && projects.length === 0) {
-          console.log('projects ANTES DEL FETCHING', projects);
-
-          const AllProjects = await projectService.getProjects();
-          console.log(projects);
-          setProjects(AllProjects);
-          console.log(userId);
-        }
-      } catch (e: any) {
-        console.log(e);
-        navigate('/auth/login');
-      }
-    };
-    getTodos();
-  }, [userId]);
   console.log(projects);
   return (
     <ul>
-      {projects ? (
+      {!isLoading ? (
         projects.map((project: Project) => {
           return <DisplayProject key={project.id} project={project} />;
         })

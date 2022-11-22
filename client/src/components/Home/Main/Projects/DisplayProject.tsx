02916eab2c36d9b3f5c4f7225f5177b9c74c82fc
srsebbi <csperichon@gmail.com>
2022-11-21 21:05:28 -0300

    Added handleDelete functionality for deleting a project

--- client/src/components/Home/Main/Projects/DisplayProject.tsx
+++ client/src/components/Home/Main/Projects/DisplayProject.tsx
@@ -1,4 +1,24 @@
 import { Project } from '../../../../types';
+import { useDeleteProject } from '../../../hooks/useDeleteProject';
+import styles from './projects.module.css';
 export const DisplayProject = ({ project }: { project: Project }) => {
-  return <li>{project.title}</li>;
+  const { mutate, isError, error, isLoading } = useDeleteProject();
+  const handleDelete = () => {
+    if (window.confirm('Are you sure?')) {
+      mutate(project.id);
+    } else {
+      console.log('Cancelled');
+    }
+  };
+  return (
+    <li>
+      {!isLoading ? (
+        <div className={styles.projectCard}>
+          Title:{project.title} <button onClick={handleDelete}>Delete</button>
+        </div>
+      ) : (
+        <p>Deleting!</p>
+      )}
+    </li>
+  );
 };

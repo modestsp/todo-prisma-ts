c5f2e0386bb99ab843b20f6893052f0fe3543665
srsebbi <csperichon@gmail.com>
2022-11-21 21:02:55 -0300

    Added updateTodo input type

--- client/src/types.ts
+++ client/src/types.ts
@@ -2,11 +2,14 @@ import { TypeOf } from 'zod';
 import { createSessionSchema } from './components/LogIn/LogInForm';
 import { createUserSchema } from './components/SignUp/SignUp';
 import { createTodoSchema } from './components/Home/Main/Todos/CreateTodoForm';
+import { createProjectSchema } from './components/Home/Main/Projects/CreateProjectForm';
+import { updateTodoSchema } from './components/Home/Main/Todos/UpdateTodo';
 
 export type CreateSessionInput = TypeOf<typeof createSessionSchema>;
 export type CreateUserInput = TypeOf<typeof createUserSchema>;
 export type CreateTodoInput = TypeOf<typeof createTodoSchema>;
-
+export type CreateProjectInput = TypeOf<typeof createProjectSchema>;
+export type UpdateTodoInput = TypeOf<typeof updateTodoSchema>;
 export interface User {
   id: string;
   username: string;

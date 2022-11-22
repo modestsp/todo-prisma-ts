4ada876cb78e9141b93e998a1af64e7bdf3d6c89
srsebbi <csperichon@gmail.com>
2022-11-21 21:02:11 -0300

    Added remove todo button and functionality

--- client/src/components/Home/Main/Todos/DisplayTodo.tsx
+++ client/src/components/Home/Main/Todos/DisplayTodo.tsx
@@ -1,9 +1,18 @@
 import { Todo } from '../../../../types';
 import styles from './todos.module.css';
 import { useDeleteTodo } from '../../../hooks/useDeleteTodo';
+import { TodoInfo } from './TodoInfo';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+import { UpdateTodoForm } from './UpdateTodo';
 
 export const DisplayTodo = ({ todo }: { todo: Todo }) => {
   const { mutate, isError, isLoading, error, isSuccess } = useDeleteTodo();
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const close = () => setModalOpen(false);
+  const open = () => setModalOpen(true);
+
   const handleDelete = () => {
     if (window.confirm('Are you sure?')) {
       mutate(todo.id);
@@ -11,14 +20,43 @@ export const DisplayTodo = ({ todo }: { todo: Todo }) => {
       console.log('Cancelled');
     }
   };
+
+  {
+    /* <motion.button
+whileHover={{ scale: 1.1 }}
+whileTap={{ scale: 0.9 }}
+onClick={() => (modalOpen ? close() : open())}
+>
+Create todo
+</motion.button>
+<AnimatePresence initial={false} mode="wait" onExitComplete={() => null}>
+{modalOpen && (
+  <Modal handleClose={close}>
+    <CreateTodoForm />
+  </Modal>
+)}
+</AnimatePresence> */
+  }
+
   return (
     <li className={styles.todoCard}>
       {!isLoading ? (
-        <div>
-          {' '}
-          <p>Description</p>
-          {todo.description} | {todo.endsAt}
-          <button onClick={handleDelete}>Remove Todo</button>
+        <div className={styles.todoInfoContainer}>
+          <TodoInfo todo={todo} />
+          {/* <motion.button onClick={handleUpdate} whileTap={{ scale: 0.9 }}>
+            Update
+          </motion.button> */}
+          <motion.button
+            onClick={handleDelete}
+            whileHover={{
+              scale: 1.1,
+              boxShadow: '1px 1px 0 rgba(0, 0, 0, 0.2)',
+            }}
+            whileTap={{ scale: 0.9 }}
+            className={styles.removeButton}
+          >
+            X
+          </motion.button>
         </div>
       ) : (
         <p>Deleting</p>

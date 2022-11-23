53d580950014f35dcd3490b88a3d6f45b11a280f
srsebbi <csperichon@gmail.com>
2022-11-23 19:27:56 -0300

    Added accordion functionality for todo list in project

--- client/src/components/Home/Main/Projects/DisplayProject.tsx
+++ client/src/components/Home/Main/Projects/DisplayProject.tsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
 import { Project } from '../../../../types';
 import { useDeleteProject } from '../../../hooks/useDeleteProject';
 import styles from './projects.module.css';
+import { motion, AnimatePresence } from 'framer-motion';
+import { CreateTodoForm } from '../Todos/CreateTodoForm';
+import { Modal } from '../../Modal';
+import { DisplayTodo } from '../Todos/DisplayTodo';
+import { ProjectHeader } from './ProjectHeader';
+
 export const DisplayProject = ({ project }: { project: Project }) => {
   const { mutate, isError, error, isLoading } = useDeleteProject();
+  const [modalOpen, setModalOpen] = useState(false);
+  const [expanded, setExpanded] = useState<boolean>(false);
+
+  const close = () => setModalOpen(false);
+  const open = () => setModalOpen(true);
+
   const handleDelete = () => {
     if (window.confirm('Are you sure?')) {
       mutate(project.id);
@@ -10,15 +23,72 @@ export const DisplayProject = ({ project }: { project: Project }) => {
       console.log('Cancelled');
     }
   };
+
   return (
-    <li>
+    <div>
       {!isLoading ? (
         <div className={styles.projectCard}>
-          Title:{project.title} <button onClick={handleDelete}>Delete</button>
+          {/* PROJECT HEADER */}
+          <motion.header
+            initial={false}
+            className={expanded ? styles.accordionOpen : styles.accordionClosed}
+            onClick={() => setExpanded(expanded ? false : true)}
+          >
+            {project.title}
+            <button>Update Project</button>
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              onClick={() => (modalOpen ? close() : open())}
+            >
+              Add todo
+            </motion.button>
+            <AnimatePresence
+              initial={false}
+              mode="wait"
+              onExitComplete={() => null}
+            >
+              {modalOpen && (
+                <Modal handleClose={close}>
+                  <CreateTodoForm projectId={project.id} />
+                </Modal>
+              )}
+            </AnimatePresence>
+            <button onClick={handleDelete}>Delete</button>
+          </motion.header>
+          {/* TODOS LIST */}
+          <AnimatePresence initial={false}>
+            {expanded && (
+              <motion.section
+                key="content"
+                initial="collapsed"
+                animate="open"
+                exit="collapsed"
+                variants={{
+                  open: { opacity: 1, height: 'auto' },
+                  collapsed: { opacity: 0, height: 0 },
+                }}
+                transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
+                className={styles.todoList}
+              >
+                <div>
+                  {project.todos?.map((todo) => {
+                    return (
+                      <DisplayTodo
+                        key={todo.id}
+                        todo={todo}
+                        projectId={project.id}
+                      />
+                    );
+                  })}
+                </div>
+              </motion.section>
+            )}
+          </AnimatePresence>
         </div>
       ) : (
         <p>Deleting!</p>
       )}
-    </li>
+    </div>
   );
 };
--- client/src/components/Home/Main/Projects/Projects.tsx
+++ client/src/components/Home/Main/Projects/Projects.tsx
@@ -2,13 +2,37 @@ import { Project } from '../../../../types';
 import { DisplayProject } from './DisplayProject';
 import { useGetProjects } from '../../../hooks/useGetProjects';
 import { CreateProjectForm } from './CreateProjectForm';
-
+import styles from './projects.module.css';
+import { useState } from 'react';
+import { Modal } from '../../Modal';
+import { motion, AnimatePresence } from 'framer-motion';
 export const Projects = () => {
   const { data: projects, isError, error, isLoading } = useGetProjects();
+  const [modalOpen, setModalOpen] = useState(false);
 
-  console.log(projects);
+  const close = () => setModalOpen(false);
+  const open = () => setModalOpen(true);
+
   return (
-    <ul>
+    <div className={styles.projectsContainer}>
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        onClick={() => (modalOpen ? close() : open())}
+      >
+        Create Project
+      </motion.button>
+
+      <AnimatePresence initial={false} mode="wait" onExitComplete={() => null}>
+        {modalOpen && (
+          <Modal handleClose={close}>
+            <CreateProjectForm
+              modalOpen={modalOpen}
+              setModalOpen={setModalOpen}
+            />
+          </Modal>
+        )}
+      </AnimatePresence>
       {!isLoading ? (
         projects.map((project: Project) => {
           return <DisplayProject key={project.id} project={project} />;
@@ -16,7 +40,6 @@ export const Projects = () => {
       ) : (
         <p>Loading</p>
       )}
-      <CreateProjectForm />
-    </ul>
+    </div>
   );
 };

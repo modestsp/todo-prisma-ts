9f6ad812085cdc5fd82bb6e4e0fb12f881097b38
srsebbi <csperichon@gmail.com>
2022-11-28 16:10:07 -0300

    Added welcome page route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,15 @@ import { Routes, Route } from 'react-router-dom';
 import { LogIn } from './components/LogIn/LogIn';
 import { SignUp } from './components/SignUp/SignUp';
 import './App.css';
-import { NotFound } from './NotFound';
+import { NotFound } from './components/NotFound';
+import { Welcome } from './components/Welcome/Welcome';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Welcome />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/auth/login" element={<LogIn />} />
         <Route path="/auth/sign-up" element={<SignUp />} />
         <Route path="*" element={<NotFound />} />

177a311a415849206cdd167930f1fb6ec6a11a8c
srsebbi <csperichon@gmail.com>
2022-11-28 16:10:34 -0300

    Added loader

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -4,19 +4,39 @@ import { Footer } from './Footer/Footer';
 import styles from './home.module.css';
 import { useGetCurrentUser } from '../hooks/useGetCurrentUser';
 import { useNavigate } from 'react-router-dom';
-export const Home = () => {
-  const { data, isLoading, isError, error } = useGetCurrentUser();
-  const navigate = useNavigate();
+import { useEffect, useState } from 'react';
+import { Loader } from '../utils/Loader';
 
-  if (isError) {
-    console.log(error);
-    navigate('/auth/login');
-  }
-  return (
+const style = {
+  borderColor: 'white transparent white transparent',
+  width: '60px',
+  height: '60px',
+};
+
+export const Home = () => {
+  const [errorMsg, setErrorMsg] = useState<any>(null);
+  const { data: currentUser, isLoading, isError, error } = useGetCurrentUser();
+  const navigate = useNavigate();
+  console.log('islOADING', isLoading);
+  useEffect(() => {
+    if (isError && !isLoading) {
+      setErrorMsg(error);
+      console.log(error);
+      navigate('/auth/login');
+    }
+  }, [currentUser, navigate, isError, error, isLoading]);
+
+  return !isLoading ? (
     <div className={styles.container}>
+      <p>{errorMsg}</p>
+
       <Header />
       <Main />
       <Footer />
+    </div>
+  ) : (
+    <div className={styles.loader}>
+      <Loader style={style} />
     </div>
   );
 };

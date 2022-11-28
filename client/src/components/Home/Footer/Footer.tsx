44fcad88c586680654d4744d898704dae1bef60f
srsebbi <csperichon@gmail.com>
2022-11-28 16:10:45 -0300

    Added personal info

--- client/src/components/Home/Footer/Footer.tsx
+++ client/src/components/Home/Footer/Footer.tsx
@@ -1,8 +1,13 @@
+import githubIcon from '../../../assets/githubIcon.svg';
 import styles from './footer.module.css';
+
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>Footer</p>
+      <a href="https://github.com/srsebbi">
+        <img src={githubIcon} alt="go to my personal github page" />
+      </a>
+      <p className={styles.phrase}> Sebastian Perichón © 2022</p>
     </footer>
   );
 };

67371165a21967eebe737e6a746cf597c863e41a
srsebbi <csperichon@gmail.com>
2022-11-29 17:35:11 -0300

    Added useEffect

--- client/src/components/Welcome/Welcome.tsx
+++ client/src/components/Welcome/Welcome.tsx
@@ -1,15 +1,40 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Footer } from '../Home/Footer/Footer';
+import { useGetCurrentUser } from '../hooks/useGetCurrentUser';
 import { Header } from './Header';
+import { motion } from 'framer-motion';
 import styles from './welcome.module.css';
-
+import { buttonDropIn } from '../../utils/animations';
 // Si user entra logeado redirect to home
 
 export const Welcome = () => {
+  const { data: currentUser } = useGetCurrentUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (currentUser) {
+      navigate('/home');
+    }
+  });
+
   return (
     <div className={styles.container}>
       <Header />
-      <p>Welcome!</p>
-      <button>Get Started!</button>
+      <div className={styles.hero}>
+        <p className={styles.heroMsg}>
+          A<span> simple </span>todo App
+        </p>
+        <motion.button
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          variants={buttonDropIn}
+          className={styles.getStarted}
+        >
+          Get started
+        </motion.button>
+      </div>
       <Footer />
     </div>
   );

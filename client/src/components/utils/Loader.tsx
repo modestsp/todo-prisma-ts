1f13258be2a27bda76868dd105eeaa1985b46178
srsebbi <csperichon@gmail.com>
2022-11-29 17:34:25 -0300

    Removed unused logs

--- client/src/components/utils/Loader.tsx
+++ client/src/components/utils/Loader.tsx
@@ -1,7 +1,6 @@
 import styles from './utils.module.css';
 
 export const Loader = (props: any) => {
-  console.log('PROPS');
   return (
     <div className={styles.loaderContainer}>
       <div style={props.style} className={styles.spinner}></div>

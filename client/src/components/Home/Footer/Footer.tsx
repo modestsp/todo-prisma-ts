866dc67f8170b7ffcad26f2aeb0a3ffb1c4ec3eb
srsebbi <csperichon@gmail.com>
2022-11-29 17:27:23 -0300

    Added animation

--- client/src/components/Home/Footer/Footer.tsx
+++ client/src/components/Home/Footer/Footer.tsx
@@ -1,13 +1,21 @@
 import githubIcon from '../../../assets/githubIcon.svg';
+import { motion } from 'framer-motion';
+import { footerBottomToTop } from '../../../utils/animations';
 import styles from './footer.module.css';
 
 export const Footer = () => {
   return (
-    <footer className={styles.footer}>
+    <motion.footer
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      variants={footerBottomToTop}
+      className={styles.footer}
+    >
       <a href="https://github.com/srsebbi">
         <img src={githubIcon} alt="go to my personal github page" />
       </a>
-      <p className={styles.phrase}> Sebastian Perichón © 2022</p>
-    </footer>
+      <p className={styles.phrase}> Sebastián Perichón © 2022</p>
+    </motion.footer>
   );
 };
